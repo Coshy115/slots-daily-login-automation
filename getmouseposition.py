b203386fca5a94d4031f5c01e1ce6af341ab9a6a
Coshy <codydls8988@gmail.com>
2025-02-08 17:47:35 -0600

    Delete getmouseposition.py

--- getmouseposition.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-
-print("Place your mouse over the desired location and press Enter...\n")
-
-input() # Wait for the user to press Enter
-
-while True:
-    print(pyautogui.position()) # Print the mouse position
-    input()
