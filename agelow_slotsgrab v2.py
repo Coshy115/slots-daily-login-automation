e49a1a4d1251ce688ae4cbd5b9a060cfc2525a13
Coshy <codydls8988@gmail.com>
2025-02-21 11:57:02 -0600

    Update agelow_slotsgrab v2.py

--- agelow_slotsgrab v2.py
+++ agelow_slotsgrab v2.py
@@ -8,7 +8,7 @@ def warning():
     print("Warning: Automation script will take control of your mouse and keyboard in approximately 10 minutes:\n")
     for i in range(1, 101):
         print(f"\r|{i * '█'}{(100 - i) * ' '}| {i}/100", end="    ")
-        time.sleep(0.1)
+        time.sleep(6)
     print("\nScript executing now...\n")
 
 def pesky_popups(x, y):
@@ -18,13 +18,31 @@ def pesky_popups(x, y):
 def PlayFame():
     # Redirect to PlayFame in new tab
     pyautogui.hotkey('ctrl', 't')
-    pyautogui.write("https://www.playfame.com/gold-coins")
+    pyautogui.write("https://www.playfame.com/lobby")
     pyautogui.press('enter')
     time.sleep(10)
+
+    # Policy Update
+    pyautogui.click(842, 628)
+    time.sleep(1)
+    pyautogui.click(1071, 678)
+    time.sleep(3)
 
     # Close popups
     pesky_popups(1236, 185)
     time.sleep(2)
+
+    # Get Coins
+    pyautogui.click(1710, 152)
+    time.sleep(5)
+
+    # Close popups
+    pesky_popups(1236, 185)
+    time.sleep(2)
+
+    # Scroll to daily bonus
+    pyautogui.click(1536, 440, clicks=10, interval=0.5)
+    time.sleep(1)
 
     # Claim button
     pyautogui.click(1472, 527)
@@ -45,28 +63,40 @@ def PlayFame():
 def McLuck():
     # Redirect to McLuck Slots in new tab
     pyautogui.hotkey('ctrl', 't')
-    pyautogui.write("https://www.mcluck.com/gold-coins")
+    pyautogui.write("https://www.mcluck.com/home")
     pyautogui.press('enter')
     time.sleep(10)
+
+    # Policy Update
+    pyautogui.click(956, 659)
+    time.sleep(3)
+
+    # Close popups
+    pesky_popups(1639, 433)
+    time.sleep(2)
+
+    # Get Coins
+    pyautogui.click(79, 270)
+    time.sleep(5)
 
     # Close popups
     pesky_popups(1639, 433)
     time.sleep(2)
 
     # Scroll to daily bonus
-    pyautogui.click(1505, 536)
-    time.sleep(5)
+    pyautogui.click(1505, 536, clicks=10, interval=0.5)
+    time.sleep(1)
 
     # Claim button
     pyautogui.click(1373, 582)
     time.sleep(5)
 
     # Claim button
-    pyautogui.click(962, 777)
+    pyautogui.click(965, 725)
     time.sleep(5)
 
     # Captcha
-    pyautogui.click(835, 782)
+    pyautogui.click(835, 730)
     time.sleep(5)
 
     # Close tab
@@ -85,7 +115,7 @@ pyautogui.press('x')
 time.sleep(2)
 
 # Execute tasks
-PlayFame()
+#PlayFame()
 McLuck()
 
 # Close Chrome
